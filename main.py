e79a6169e8ae42e96ba00708e77833398cac20de
Udara Wickramasinghe <65730273+WickyUdara@users.noreply.github.com>
2025-10-20 23:26:47 +0530

    draw annotations complete

---
+++ main.py
@@ -0,0 +1,29 @@
+from utils import read_video, save_video
+from trackers import Tracker
+import cv2
+
+def main():
+    video_frames = read_video('input_videos/video1.mp4')
+    tracker = Tracker('models/best.pt')
+    tracks = tracker.get_object_tracks(video_frames,
+                                       read_from_stub=True,
+                                       stub_path='stubs/tracks_stub.pkl')
+    
+
+    for track_id,player in tracks['players'][0].items():
+        bbox = player['bbox']
+        frame = video_frames[0]
+
+        cropped_image = frame[int(bbox[1]):int(bbox[3]), int(bbox[0]):int(bbox[2])]
+
+        cv2.imwrite('output_videos/cropped_player.jpg', cropped_image)
+
+        break
+        
+    
+    output_video_frames = tracker.draw_annotations(video_frames, tracks)
+
+    save_video(output_video_frames, 'output_videos/output_video.avi')
+    print("Output video saved to output_videos/output_video.avi")
+if __name__ == "__main__":
+    main()
